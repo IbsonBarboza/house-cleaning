4040fe92a0cee9b97f1f9dca8092d6cc1828d4a7
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2024-02-06 14:44:02 -0500

    home-finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: '#f5f5f5',
+        bgNav: '#e0e0e0',
+        textColor: '#333333',
+        buttColor: '#4cb5f5',
+        highlightText: '#66cc99',
+        bgButtons: '#d8f3e5',
+      },
+    },
   },
   plugins: [],
 }
